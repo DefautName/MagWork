f1dceb89287d6b3c855bc7b9007ddff35ae1aa10
Ilia Kuzin <ilyakuzin20013107@gmail.com>
2025-05-12 22:03:29 +0300

    Add gitignore

--- build/Desktop_Qt_6_7_0_MinGW_64_bit-Debug/qtcsettings.cmake
+++
@@ -1,8 +0,0 @@
-# This file is managed by Qt Creator, do not edit!
-
-set("CMAKE_AR" "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/bin/HostX64/x64/ar.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_CXX_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/bin/HostX64/x64/cl.exe" CACHE "STRING" "" FORCE)
-set("CMAKE_C_COMPILER" "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/bin/HostX64/x64/cl.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_LINKER" "C:/Program Files/Microsoft Visual Studio/2022/Community/VC/Tools/MSVC/14.41.34120/bin/HostX64/x64/ld.exe" CACHE "FILEPATH" "" FORCE)
-set("CMAKE_PREFIX_PATH" "C:/Qt/6.8.2/msvc2022_64;C:/Qt/6.7.0/mingw_64" CACHE "PATH" "" FORCE)
-set("QT_QMAKE_EXECUTABLE" "C:/Qt/6.8.2/msvc2022_64/bin/qmake.exe" CACHE "FILEPATH" "" FORCE)
